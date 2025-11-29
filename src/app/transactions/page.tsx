c78e58cdfd9f37ab074f6c687e6f05e2f591b6b6
superkat64 <kat.piette@gmail.com>
2025-11-29 12:20:41 -0500

    refactor: name nit

--- src/app/transactions/page.tsx
+++ src/app/transactions/page.tsx
@@ -1,9 +1,10 @@
+// app/transactions/page.tsx
 import TransactionTable from "@/components/transaction-table";
 import Link from "next/link";
 import { listTransactions } from "@/app/transactions/actions";
 
 export default async function TransactionsPage() {
-  const tx = await listTransactions();
+  const transactions = await listTransactions();
 
   return (
     <div className="p-8">
@@ -11,7 +12,7 @@ export default async function TransactionsPage() {
         <h1 className="text-2xl font-bold mb-4">Recent Transactions</h1>
         <Link href="/transactions/new" className="px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700">Add Transaction</Link>
       </div>
-      <TransactionTable transactions={tx} />
+      <TransactionTable transactions={transactions} />
     </div>
   );
 }
