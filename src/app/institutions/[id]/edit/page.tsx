c5c2ee486901067c1476081c209d54cbcb7b1e57
superkat64 <kat.piette@gmail.com>
2025-12-03 10:26:07 -0500

    styles: add uniform styling

--- src/app/institutions/[id]/edit/page.tsx
+++ src/app/institutions/[id]/edit/page.tsx
@@ -1,5 +1,7 @@
 // app/institutions/[id]/edit/page.tsx
 import InstitutionForm from "@/components/institution-form";
+import Link from "next/link";
+import { ChevronLeft } from "lucide-react";
 import { getInstitutionById } from "@/app/institutions/actions";
 import { notFound } from "next/navigation";
 
@@ -7,13 +9,19 @@ export default async function EditInstitutionPage({ params }: { params: { id: st
   const institution = await getInstitutionById(params.id);
   if (!institution) notFound();
   return (
-    <div className="min-h-screen flex flex-col items-center justify-center p-8">
-      <div className="w-full max-w-md">
-        <div>
-          <h1 className="text-4xl font-bold text-center mb-8">Edit Institution: {institution.name}</h1>
-          <InstitutionForm institution={institution} />
+    <>
+      <Link href="/institutions" className="flex flex-row mt-4 ml-4">
+        <ChevronLeft />
+        Back
+      </Link>
+      <div className="flex flex-col items-center justify-center sm:p-20 font-[family-name:var(--font-geist-sans)]">
+        <div className="w-full max-w-md">
+          <div>
+            <h1 className="text-4xl font-bold text-center mb-8">Edit Institution: {institution.name}</h1>
+            <InstitutionForm institution={institution} />
+          </div>
         </div>
       </div>
-    </div>
+    </>
   )
 }
